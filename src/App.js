b79b2e08d44fb64a6110a42c529b2dfe48a77c72
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-09-23 19:46:01 +0800

    Added Navigation, Header, IndexSection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./styles/main.css"
+
+import Navigation from "./components/Navigation/Navigation"
+import Header from './components/Header/Header'
+import IndexSection from "./components/IndexSection/IndexSection"
 import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
+      <Header/>
+      <IndexSection/>
       <Footer/>
     </div>
   );
