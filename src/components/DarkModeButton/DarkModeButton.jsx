50fd9639be6e8ed0bfa0905288a5005d8ea08d87
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-09-25 20:13:43 +0800

    Added dark mode switching

--- src/components/DarkModeButton/DarkModeButton.jsx
+++ src/components/DarkModeButton/DarkModeButton.jsx
@@ -1,13 +1,32 @@
 import './DarkModeButton.css'
+import { useEffect, useState } from 'react'
 
 import SunImg from './img/sun.svg'
 import MoonImg from './img/moon.svg'
 
 function DarkModeButton(){
+    const lightState = "dark-mode-btn"
+    const darkState = lightState + " dark-mode-btn--active"
+    
+    const [isDark, setIsDark] = useState(false)
+
+    useEffect(() => { 
+
+        if(isDark)
+            document.body.classList.add("dark");
+        else
+            document.body.classList.remove("dark");
+        
+    }, [isDark])
+
+    function switchColorMode(){
+        setIsDark(!isDark)
+    }
+
     return(
-        <button className="dark-mode-btn">
-            <img src={SunImg} alt="Light mode" className="dark-mode-btn__icon"/>
-            <img src={MoonImg} alt="Dark mode" className="dark-mode-btn__icon"/>
+        <button className={isDark ? darkState : lightState} onClick={switchColorMode}>
+            <img src={SunImg} alt="Light mode" className="dark-mode-btn__icon" />
+            <img src={MoonImg} alt="Dark mode" className="dark-mode-btn__icon" />
         </button>
     );
 }
